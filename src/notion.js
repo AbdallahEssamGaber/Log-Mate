15f3d51fa50a886173670bd0d42bf3fbc37e809b
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-08-22 22:22:57 +0300

    editing Notion.js Check-in to accept rollups and more

--- src/notion.js
+++ src/notion.js
@@ -5,41 +5,83 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-const database_id = process.env.NOTION_DB_ID;
+const checkIn_database_id = process.env.NOTION_CHECKIN_DB_ID;
+const members_database_id = process.env.NOTION_MEMBERS_DB_ID;
 
-//create a notion page
-module.exports = async (fields) => {
+const createMember = async (name) => {
   try {
     const response = await notion.pages.create({
       parent: {
         type: "database_id",
-        database_id: database_id,
+        database_id: members_database_id,
       },
       properties: {
         Name: {
           title: [
             {
               text: {
-                content: fields.name,
+                content: name,
               },
             },
           ],
         },
-        "pervious workday": {
-          rich_text: [
-            {
-              text: {
-                content: fields.perviousWorkday,
-              },
-            },
-          ],
+      },
+    });
+    return response.id;
+  } catch (error) {
+    console.error(error.body);
+  }
+};
+
+const isAval = async (name) => {
+  try {
+    const response = await notion.databases.query({
+      database_id: members_database_id,
+      filter: {
+        property: "Name",
+        rich_text: {
+          equals: name,
         },
+      },
+    });
+    if (!response.results.length) {
+      return false;
+    }
+
+    return response.results[0].id;
+  } catch (error) {
+    console.error(error.body);
+  }
+};
+
+//create a notion page
+module.exports = async (fields) => {
+  try {
+    //the id for the rollup page
+    let id = await isAval(fields.name);
+    if (!id) {
+      id = await createMember(fields.name);
+    }
+
+    const response = await notion.pages.create({
+      parent: {
+        type: "database_id",
+        database_id: checkIn_database_id,
+      },
+      properties: {
         "today work": {
-          rich_text: [
+          title: [
             {
               text: {
                 content: fields.todayWork,
               },
+            },
+          ],
+        },
+        "Team Member Relation": {
+          relation: [
+            {
+              id: id,
             },
           ],
         },
@@ -48,6 +90,15 @@ module.exports = async (fields) => {
             {
               text: {
                 content: fields.blockers,
+              },
+            },
+          ],
+        },
+        "Discord Username": {
+          rich_text: [
+            {
+              text: {
+                content: fields.username,
               },
             },
           ],
