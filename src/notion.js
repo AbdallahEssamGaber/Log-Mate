90a2a00ae798351457bc030295918a048cf09a1c
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-08-23 08:15:23 +0300

    Adding new check-out commnd -passing

--- src/notion.js
+++ src/notion.js
@@ -5,15 +5,15 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-const checkIn_database_id = process.env.NOTION_CHECKIN_DB_ID;
-const members_database_id = process.env.NOTION_MEMBERS_DB_ID;
+const {NOTION_CHECKIN_DB_ID,  NOTION_MEMBERS_DB_ID, NOTION_CHECKOUT_DB_ID} = process.env;
+
 
 const createMember = async (name) => {
   try {
     const response = await notion.pages.create({
       parent: {
         type: "database_id",
-        database_id: members_database_id,
+        database_id: NOTION_MEMBERS_DB_ID,
       },
       properties: {
         Name: {
@@ -36,7 +36,7 @@ const createMember = async (name) => {
 const isAval = async (name) => {
   try {
     const response = await notion.databases.query({
-      database_id: members_database_id,
+      database_id: NOTION_MEMBERS_DB_ID,
       filter: {
         property: "Name",
         rich_text: {
@@ -66,7 +66,7 @@ module.exports.createCheckIn = async (fields) => {
     const response = await notion.pages.create({
       parent: {
         type: "database_id",
-        database_id: checkIn_database_id,
+        database_id: NOTION_CHECKIN_DB_ID,
       },
       properties: {
         "today work": {
@@ -93,6 +93,53 @@ module.exports.createCheckIn = async (fields) => {
               },
             },
           ],
+        },
+        "Discord Username": {
+          rich_text: [
+            {
+              text: {
+                content: fields.username,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(error.body);
+  }
+};
+
+
+
+module.exports.createCheckOut = async (fields) => {
+  try {
+    //the id for the rollup page
+    let id = await isAval(fields.name);
+    if (!id) {
+      id = await createMember(fields.name);
+    }
+
+    const response = await notion.pages.create({
+      parent: {
+        type: "database_id",
+        database_id: NOTION_CHECKOUT_DB_ID,
+      },
+      properties: {
+        "Description": {
+          title: [
+            {
+              text: {
+                content: fields.description,
+              },
+            },
+          ],
+        },
+        "Start Time": {
+          date: {
+            start: ,
+          }
         },
         "Discord Username": {
           rich_text: [
