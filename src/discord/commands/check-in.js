f090a86d74fd056236a5a750828dfef34e855202
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-09-02 13:00:55 +0300

    new beta folder structure skelton

--- src/discord/commands/check-in.js
+++ src/discord/commands/check-in.js
@@ -1,10 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
-
+const {
+  SlashCommandBuilder,
+  TextInputStyle,
+  ActionRowBuilder,
+} = require("discord.js");
+const { newModal, newInput } = require("../interactions/modalBuilder");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("check-in")
     .setDescription("Daily-Checking-In!"),
-  async execute(interaction) {
-    await interaction.reply("Checking-In....");
+  async execute(interaction, client) {
+    const modal = await newModal("dailyCheckIn", "Daily-Check-In");
+
+    const todayWork = await newInput({
+      require: true,
+      id: "todayWork",
+      label: "planning to work on today",
+      placeholder: "what are you planning to work on today",
+      style: TextInputStyle.Short,
+    });
+    modal.addComponents(new ActionRowBuilder().addComponents(todayWork));
+    await interaction.showModal(modal);
   },
 };
