685a990271318fef4ae775c0a13730bd80c441fb
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-09-04 00:21:12 +0300

    don't submit log timing on empty values

--- src/discord/utils/responses/modals/task.js
+++ src/discord/utils/responses/modals/task.js
@@ -1,16 +1,9 @@
-const {
-  ActionRowBuilder,
-  ComponentType,
-  ModalBuilder,
-  TextInputBuilder,
-  TextInputStyle,
-} = require("discord.js");
+const { ActionRowBuilder, ComponentType } = require("discord.js");
 const newButton = require("../../components/buttonBuilder");
 const {
   newStringSelectMenuBuilder,
   newStringSelectMenuOptionBuilder,
 } = require("../../components/selectMenuBuilder");
-// const { newModal, newInput } = require("../../components/modalBuilder.js");
 const { addMins, subMins } = require("../../../../functions/general/timeCalc");
 
 module.exports = {
@@ -60,40 +53,10 @@ module.exports = {
       style: "primary",
     });
 
-    // const modal = await newModal("taskFinishedEntry", "Log Finished Task");
-    // const startTimeFieldInput = {
-    //   required: true,
-    //   id: "startTimeInput",
-    //   label: `When did you start working on ${taskName}?`,
-    //   placeholder: `12-hours system plz, include "am" or "pm" at the end.`,
-    //   minLength: 6,
-    //   maxLength: 8,
-    //   style: "short",
-    // };
-    // const endTimeFieldInput = {
-    //   required: true,
-    //   id: "endTimeInput",
-    //   placeholder: `12-hours system plz, include "am" or "pm" at the end.`,
-    //   minLength: 6,
-    //   maxLength: 8,
-    //   label: `When did you finish working on ${taskName}?`,
-    //   style: "short",
-    // };
-
-    // const startTimeValue = await newInput(startTimeFieldInput);
-    // const endTimeValue = await newInput(endTimeFieldInput);
-
     const row = new ActionRowBuilder().addComponents(startTimeSelect);
     const row2 = new ActionRowBuilder().addComponents(endTimeSelect);
     const row3 = new ActionRowBuilder().addComponents(addButton);
     const row4 = new ActionRowBuilder().addComponents(confirm);
-
-    // const startTimeActionRow = new ActionRowBuilder().addComponents(
-    //   startTimeValue
-    // );
-    // const endTimeActionRow = new ActionRowBuilder().addComponents(endTimeValue);
-    //Add inputs to the modal
-    // modal.addComponents(startTimeActionRow, endTimeActionRow);
 
     const response = await interaction.reply({
       content: `\`\`\`Task: ${taskName}✅\`\`\`
@@ -122,15 +85,12 @@ Chose It's Start and End Time For The Task Below, Please.`,
     );
     const taskTimes = {
       disabled: false,
-      startTimeSelector: null,
-      endTimeSelector: null,
     };
     collectorSelect.on("collect", async (i) => {
       const customId = i.customId;
       const selection = i.values[0];
-      if (taskTimes.hasOwnProperty(customId)) {
-        taskTimes[customId] = selection;
-      }
+      taskTimes[customId] = selection;
+
       await i.reply({
         content: `*you chose ${selection} from the ${interaction.customId} successfully!*`,
         ephemeral: true,
@@ -148,14 +108,9 @@ Chose It's Start and End Time For The Task Below, Please.`,
 
     collectorButton.on("end", async () => {
       collectorSelect.stop();
-
-      console.log("ENDED Selector.");
-      // console.log(collected);
       if (taskName.disabled == true) {
-        console.log("Should be in");
         await response.delete();
       } else {
-        console.log("Shouldn'tttt be in");
         if (
           taskTimes.hasOwnProperty("startTimeSelector") &&
           taskTimes.hasOwnProperty("endTimeSelector") &&
@@ -163,11 +118,20 @@ Chose It's Start and End Time For The Task Below, Please.`,
         ) {
           await response.edit({
             content: `Way to gooo👏👏
-  you finished ${taskName} from ${taskTimes.startTimeSelector} until ${taskTimes.endTimeSelector}`,
+You finished ${taskName} from ${taskTimes.startTimeSelector} until ${taskTimes.endTimeSelector}`,
+            components: [],
+          });
+        } else if (
+          !taskTimes.hasOwnProperty("startTimeSelector") &&
+          !taskTimes.hasOwnProperty("endTimeSelector") &&
+          taskTimes.disabled !== null
+        ) {
+          await response.edit({
+            content: "**Please select values!**",
             components: [],
           });
         } else {
-          await response.edit({ content: "DONE.", components: [] });
+          await response.delete();
         }
       }
     });
