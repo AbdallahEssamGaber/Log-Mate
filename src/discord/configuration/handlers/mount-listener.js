b75bb0cfcbf2cbbcf7153695dda231376e08b4ae
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-09-04 23:59:01 +0300

    deleteing unseful folders

--- src/discord/configuration/handlers/mount-listener.js
+++ src/discord/configuration/handlers/mount-listener.js
@@ -1,7 +1,5 @@
 module.exports = async (client, files) => {
   for (const file of files) {
-    //Excluding the classes file
-    // if (file.includes("classes")) continue;
     const event = require(file);
     if (event.once) {
       //When the client is ready, run this code (only once)
