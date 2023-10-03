fee3784cafb862fb7fa4777643e18888739a0e14
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-10-03 17:12:36 +0300

    updating timeCalc code

--- src/functions/general/timeCalc.js
+++ src/functions/general/timeCalc.js
@@ -1,55 +1,12 @@
-module.exports.addMins = (intMin) => {
-  let dateObj = [];
-  const numberOfMlSeconds = new Date().getTime();
+const { addMinutes, format, roundToNearestMinutes } = require("date-fns");
 
-  //if you wanna add hours parse min*60
-  const addMlSeconds = intMin * 60 * 1000;
-  let newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
-
-  newDateObj.setMinutes(30, 0);
-  dateObj.push(
-    newDateObj.toLocaleTimeString("en-US", {
-      timeZone: "Africa/Dar_es_Salaam",
-      hour: "2-digit",
-      minute: "2-digit",
-    })
+module.exports = (n) => {
+  const date = new Date();
+  let newMins = addMinutes(date, n * 60);
+  const thePushUp = format(
+    roundToNearestMinutes(newMins, { nearestTo: 30 }),
+    "p"
   );
-  newDateObj.setMinutes(0, 0);
-
-  dateObj.push(
-    newDateObj.toLocaleTimeString("en-US", {
-      timeZone: "Africa/Dar_es_Salaam",
-      hour: "2-digit",
-      minute: "2-digit",
-    })
-  );
-  return dateObj;
-};
-
-module.exports.subMins = (intMin) => {
-  let dateObj = [];
-  const numberOfMlSeconds = new Date().getTime();
-
-  //if you wanna add hours parse min*60
-  const addMlSeconds = intMin * 60 * 1000;
-  let newDateObj = new Date(numberOfMlSeconds - addMlSeconds);
-  newDateObj.setMinutes(0, 0);
-
-  dateObj.push(
-    newDateObj.toLocaleTimeString("en-US", {
-      timeZone: "Africa/Dar_es_Salaam",
-      hour: "2-digit",
-      minute: "2-digit",
-    })
-  );
-  newDateObj.setMinutes(30, 0);
-  dateObj.push(
-    newDateObj.toLocaleTimeString("en-US", {
-      timeZone: "Africa/Dar_es_Salaam",
-      hour: "2-digit",
-      minute: "2-digit",
-    })
-  );
-
-  return dateObj;
+  console.log(thePushUp);
+  return thePushUp;
 };
