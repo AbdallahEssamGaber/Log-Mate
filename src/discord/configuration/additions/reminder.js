61d422e9e1d2e4ba33ae01b8d5a3e71a2d897524
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2023-12-11 01:15:01 +0200

    updating after setting timezone

--- src/discord/configuration/additions/reminder.js
+++ src/discord/configuration/additions/reminder.js
@@ -2,43 +2,45 @@ const Task = require("../../../schemas/task");
 const cron = require("cron");
 const { format, differenceInHours } = require("date-fns");
 module.exports = async (client) => {
-  // let scheduledMessage = new cron.CronJob("0 0 14-19/4 * * 0-4", async () => {
-  const date = format(new Date(), "yyyy-MM-dd");
+  let scheduledMessage = new cron.CronJob("0 0 14-19/4 * * 0-4", async () => {
+    const date = format(new Date(), "yyyy-MM-dd");
 
-  const guild = client.guilds.cache.get(process.env.GUILD_ID);
-  let resMembers = await guild.members.fetch();
-  resMembers = resMembers.filter((member) => !member.user.bot);
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    let resMembers = await guild.members.fetch();
+    resMembers = resMembers.filter((member) => !member.user.bot);
 
-  resMembers.forEach(async (member) => {
-    const userId = member.user.id;
-    const tasks = await Task.find({
-      discord_userId: userId,
-      created_time: date,
-    }).exec();
-    const undoneTasks = tasks.filter((task) => !task.done);
-    const doneTasks = tasks.filter((task) => task.done);
-    if (!tasks.length) {
-      member.user.send(`Please, type \`/log\` command to log task and time.`);
-    } else if (tasks.length == undoneTasks) {
-      member.user.send(`Please, type \`/log\` command to log task and time.`);
-    } else {
-      let endDateCounter = 0;
-      doneTasks.forEach(async (task) => {
-        const diffHours = differenceInHours(
-          new Date(),
-          new Date(task.end_time)
-        );
-        if (diffHours >= 4) {
-          endDateCounter++;
-        }
-      });
-      if (doneTasks.length == endDateCounter) {
+    resMembers.forEach(async (member) => {
+      const userId = member.user.id;
+      const tasks = await Task.find({
+        discord_userId: userId,
+        created_time: date,
+      }).exec();
+      const undoneTasks = tasks.filter((task) => !task.done);
+      const doneTasks = tasks.filter((task) => task.done);
+      if (!tasks.length) {
         member.user.send(`Please, type \`/log\` command to log task and time.`);
+      } else if (tasks.length == undoneTasks) {
+        member.user.send(`Please, type \`/log\` command to log task and time.`);
+      } else {
+        let endDateCounter = 0;
+        doneTasks.forEach(async (task) => {
+          const diffHours = differenceInHours(
+            new Date(),
+            new Date(task.end_time)
+          );
+          if (diffHours >= 4) {
+            endDateCounter++;
+          }
+        });
+        if (doneTasks.length == endDateCounter) {
+          member.user.send(
+            `Please, type \`/log\` command to log task and time.`
+          );
+        }
       }
-    }
+    });
   });
-  // });
 
-  // // When you want to start it, use:
-  // scheduledMessage.start();
+  // When you want to start it, use:
+  scheduledMessage.start();
 };
